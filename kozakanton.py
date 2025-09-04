7b5c021417035bb9eedf701ac8d6bba920d89f53
KozakAnton <kozakanton18@gmail.com>
2025-09-04 13:57:08 +0300

    Add files via upload

---
+++ kozakanton.py
@@ -0,0 +1,36 @@
+number=10
+print ('number',type(number),':',number)
+hud='yatoro'
+print ('hud',type(hud),':',hud)
+num=1.0
+print ('num',type(num),':',num)
+sharp= False
+print ('sharp',type(sharp),':',sharp)
+list =[1,1,1,1]
+print ('list',type(list),':',list)
+set ={2,2,1,'pruvet'}
+print ('set',type(set),':',set)
+core =(9,9,9)
+print('core',type(core),':',core)
+dict ={2:'hello'}
+print('dict',type(dict),':',dict)
+#змінні
+a=10
+b=12
+print('a + b=',a+b)
+#додавання
+print('a - b=',a-b)
+#віднімання
+print('a * b=',a*b)
+#множення
+print('a / b=',a/b)
+#ділення
+print('a % b=',a%b)
+#остача
+print('a  b=',a**b)
+#степінь
+print('a // b=',a//b)
+#ділення без остачі
+
+#оператори
+
