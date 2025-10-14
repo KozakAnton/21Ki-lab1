598c8c4ae6601f9894f274cd92199b5a7bd6be5f
KozakAnton <kozakanton18@gmail.com>
2025-10-14 22:57:57 +0300

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,50 @@
+# Форматує ціну у вигляді рядка з двома десятковими знаками та гривневим знаком
+def format_price(price):  # Функція1 - формат ціни
+    return f"{price:.2f} грн"
+
+
+# Перевіряє наявність товарів у магазині, повертає словник з інформацією про доступність кожного товару
+def check_availability(store, *items):  # Функція2 - чек наявності
+    return {item: (item in store and store[item][1]) for item in items}
+
+
+# Обробляє процес замовлення: перевіряє наявність товарів, розраховує загальну суму та виводить чек
+def order(store):  # Функція3 - замовлення
+    order = input("Введіть товари через пробіл: ").split()
+    check = check_availability(store, *order)
+    unavailable = [item for item, available in check.items() if not available]
+
+    if unavailable:
+        print("Замовлення скасовано! Немає:", ", ".join(unavailable))  # join обєднує в рядок через ", "
+        return
+
+    total = 0
+    print("Замовлення:")
+    for item in order:
+        price = store[item][0]
+        print(f"{item}: {format_price(price)}")
+        total += price
+
+    print("Загалом:", format_price(total))
+
+
+# Головна функція програми, яка керує основним циклом взаємодії з користувачем
+def main():  # Функція4 - головна
+    store = {"Стіл": [1599.99, True], "Стілець": [500.50, False], "Лампа": [400.50, False], "Шафа": [30199.99, True],
+             "Диван": [5800.25, True]}
+
+    while True:
+        choice = input("Оберіть дію (1 - Купити, 2 - Переглянути ціни):")
+
+        if choice == "1":
+            order(store)
+        elif choice == "2":
+            for item, (price, available) in store.items():
+                status = "+" if available else "-"
+                print(f"{item}: {format_price(price)} ({status})")
+        else:
+            print("Непрописаний варіант!")
+
+
+if __name__ == '__main__':
+    main()
